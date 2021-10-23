5ae60c983dc63ebf4d5893e35614f77b0d299c55
aayuspudasaini <aayuspudasaini@gmail.com>
2021-10-23 08:54:05 +0545

    First Code

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title>Login Page</title>
+</head>
+<body>
+    
+        <div class="col-md-3 main ">
+            <div class="container-fluid login">
+        Login Page
+            </div>
+            <div class="view ">
+                <form action="adddata.php" method="post">
+                    <label for="username"><i class="far fa-user" style="margin-right:10px"></i>Username</label><br>
+                    <input type="text " id="username" name="username" class= "form-control" required>
+                    <label for="password"><i class="fas fa-key" style="margin-right:10px;font-weight:400;"></i>Password</label><br>
+                    <input type="password" id="password" name="password" required class= "form-control">
+                    <label for="password"><i class="fas fa-key" style="margin-right:10px"></i>Re-type Password</label><br>
+                    <input type="password" id="repassword" name="repassword" required class= "form-control "> 
+                    <input type="submit" value="Save" class="btn btn-primary ">
+                </form>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
