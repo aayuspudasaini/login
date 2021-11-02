cabfcd8a1f8affec0aba2803ebbbbee2f1101a48
aayuspudasaini <aayuspudasaini@gmail.com>
2021-11-02 19:02:44 +0545

    code updated

--- adddata.php
+++ adddata.php
@@ -1,15 +1,12 @@
 <?php
+include 'dp.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 $repassword = $_POST['repassword'];
-include 'dp.php';
 $sql = "INSERT INTO usr (username, password, repassword) VALUES ('$username','$password','$repassword')";
 $result = mysqli_query ($conn,$sql);
 if ($result)
 {
-    header('location:index.php');
+    header('location:welcome.php');
 }
-
-
-
 ?>
